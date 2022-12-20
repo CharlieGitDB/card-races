b4a42718cd2d661ef09961b08afee3f3f755cfd3
CharlieGitDB <CharlieGitDB@users.noreply.github.com>
2022-12-19 20:27:23 -0600

    shorten sleep time

--- pubsub/Model/GameEventHandler.cs
+++ pubsub/Model/GameEventHandler.cs
@@ -71,7 +71,7 @@ public class GameEventHandler
       var messageData = BinaryData.FromString(JsonConvert.SerializeObject(updatedGame));
       await _actions.AddAsync(WebPubSubAction.CreateSendToGroupAction(group, messageData, WebPubSubDataType.Text));
       _logger.LogInformation("[START] Round ended..");
-      Thread.Sleep(3500);
+      Thread.Sleep(2800);
     }
 
     var declareWinner = updatedGame.GetWinningUsers((Suit)updatedGame.Winner);
