b720b213b100d76faf52d9343b35de34364bcb1e
CharlieGitDB <CharlieGitDB@users.noreply.github.com>
2023-01-24 19:59:20 -0600

    initial home and lobby page

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,18 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateComponent } from './intro/components/create/create.component';
+import { JoinComponent } from './intro/components/join/join.component';
+import { IntroPageComponent } from './intro/containers/intro-page/intro-page.component';
+import { OptionsComponent } from './intro/containers/options/options.component';
+import { NotFoundPageComponent } from './shared/containers/not-found-page/not-found-page.component';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { PlayerListComponent } from './lobby/components/player-list/player-list.component';
+import { LobbyPageComponent } from './lobby/containers/lobby-page/lobby-page.component';
+import { StartComponent } from './lobby/components/start/start.component';
+import { GamePageComponent } from './game/containers/game-page/game-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateComponent,
+    JoinComponent,
+    IntroPageComponent,
+    NotFoundPageComponent,
+    OptionsComponent,
+    LobbyPageComponent,
+    PlayerListComponent,
+    StartComponent,
+    GamePageComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatListModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
