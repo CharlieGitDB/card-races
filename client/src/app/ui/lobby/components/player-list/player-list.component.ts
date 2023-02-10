c5a7baaf90db9e1e652261d93809cced72a9657a
CharlieGitDB <CharlieGitDB@users.noreply.github.com>
2023-02-09 19:20:10 -0600

    start displaying users in group

--- client/src/app/ui/lobby/components/player-list/player-list.component.ts
+++ client/src/app/ui/lobby/components/player-list/player-list.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { selectGameUserData } from 'src/app/data/store/store';
+import { AppState } from 'src/app/data/types/AppState';
 
 @Component({
   selector: 'app-player-list',
   templateUrl: './player-list.component.html',
-  styleUrls: ['./player-list.component.scss']
+  styleUrls: ['./player-list.component.scss'],
 })
 export class PlayerListComponent {
-
+  private store: Store<AppState> = inject(Store);
+  public users$ = this.store.select(selectGameUserData);
 }
