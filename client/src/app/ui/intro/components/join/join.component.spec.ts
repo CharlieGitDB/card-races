2bfe0e632d22a0188612cb7dee7df64a9b9bd27d
CharlieGitDB <charliedeveloperaccess@gmail.com>
2023-02-24 13:15:16 -0600

    create join component tests

--- client/src/app/ui/intro/components/join/join.component.spec.ts
+++ client/src/app/ui/intro/components/join/join.component.spec.ts
@@ -2,14 +2,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { By } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { provideMockStore } from '@ngrx/store/testing';
+import { of } from 'rxjs';
+import { SUIT } from 'src/app/data/types/Suit';
+import { MOCK_GROUP_ID } from 'src/app/testing/mock';
+import { IntroFacade } from '../../facades/intro.facade';
 
 import { JoinComponent } from './join.component';
 
 describe('JoinComponent', () => {
   let component: JoinComponent;
   let fixture: ComponentFixture<JoinComponent>;
+  let mockJoinGame = jasmine.createSpy('joinGame');
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -20,8 +26,16 @@ describe('JoinComponent', () => {
         BrowserAnimationsModule,
       ],
       declarations: [JoinComponent],
-      providers: [provideMockStore()],
-    }).compileComponents();
+      providers: [
+        provideMockStore(),
+        {
+          provide: IntroFacade,
+          useValue: {
+            joinGame: mockJoinGame,
+          },
+        },
+      ],
+    });
 
     fixture = TestBed.createComponent(JoinComponent);
     component = fixture.componentInstance;
@@ -30,5 +44,84 @@ describe('JoinComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should NOT allow clicking of the join game button when there is a suit but not a groupId value and not a suit', () => {
+    const joinGameButton = fixture.debugElement.query(By.css('button'))
+      .nativeElement as HTMLButtonElement;
+
+    expect(joinGameButton.disabled).toBeTruthy();
+  });
+
+  it('should NOT allow clicking of the join game button when there is a groupId value but there isnt a suit', () => {
+    component.joinForm.setValue({
+      groupId: MOCK_GROUP_ID,
+    });
+
+    const joinGameButton = fixture.debugElement.query(By.css('button'))
+      .nativeElement as HTMLButtonElement;
+
+    expect(joinGameButton.disabled).toBeTruthy();
+  });
+
+  it('should NOT allow clicking of the join game button when there is a suit but not a groupId value', () => {
+    component.suit$ = of(SUIT.DIAMONDS);
+
+    component.joinForm.setValue({
+      groupId: null,
+    });
+
+    const joinGameButton = fixture.debugElement.query(By.css('button'))
+      .nativeElement as HTMLButtonElement;
+
+    expect(joinGameButton.disabled).toBeTruthy();
+  });
+
+  it('should allow clicking of the join game button when there is a suit and group value', () => {
+    component.suit$ = of(SUIT.DIAMONDS);
+
+    component.joinForm.setValue({
+      groupId: MOCK_GROUP_ID,
+    });
+
+    fixture.detectChanges();
+
+    const joinGameButton = fixture.debugElement.query(By.css('button'))
+      .nativeElement as HTMLButtonElement;
+
+    expect(joinGameButton.disabled).toBeFalsy();
+    expect(joinGameButton.textContent).toContain('Join Game');
+  });
+
+  it('should disable join button when loading is true', async () => {
+    component.loading = true;
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    const joinGameButton = fixture.debugElement.query(By.css('button'))
+      .nativeElement as HTMLButtonElement;
+
+    expect(joinGameButton.disabled).toBeTruthy();
+  });
+
+  it('should call submit when join game is clicked', async () => {
+    spyOn(component, 'submit').and.callThrough();
+
+    component.suit$ = of(SUIT.DIAMONDS);
+
+    component.joinForm.setValue({
+      groupId: MOCK_GROUP_ID,
+    });
+
+    fixture.detectChanges();
+
+    const joinGameButton = fixture.debugElement.query(By.css('button'))
+      .nativeElement as HTMLButtonElement;
+    joinGameButton.click();
+
+    await fixture.whenStable();
+
+    expect(component.submit).toHaveBeenCalled();
+    expect(mockJoinGame).toHaveBeenCalled();
   });
 });
