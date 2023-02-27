f8226eeca774f863040efc723d89ff5bc6ba7806
CharlieGitDB <charliedeveloperaccess@gmail.com>
2023-02-26 18:44:13 -0600

    write suit seelct and intro page tests

--- client/src/app/ui/intro/components/suit-select/suit-select.component.spec.ts
+++ client/src/app/ui/intro/components/suit-select/suit-select.component.spec.ts
@@ -1,5 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { provideMockStore } from '@ngrx/store/testing';
+import { By } from '@angular/platform-browser';
+import { StoreModule } from '@ngrx/store';
+import { suitReducer, SUIT_KEY } from 'src/app/data/store/store';
+import { Suit, SUIT, suitList } from 'src/app/data/types/Suit';
 
 import { SuitSelectComponent } from './suit-select.component';
 
@@ -9,8 +12,16 @@ describe('SuitSelectComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        StoreModule.forRoot(
+          //use real store instead of mock store
+          {
+            [SUIT_KEY]: suitReducer,
+          },
+          {}
+        ),
+      ],
       declarations: [SuitSelectComponent],
-      providers: [provideMockStore()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(SuitSelectComponent);
@@ -20,5 +31,78 @@ describe('SuitSelectComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have select suit p tag', () => {
+    const p = fixture.debugElement.query(By.css('p.mat-h1')).nativeElement;
+
+    expect(p.textContent).toContain('Select Suit');
+  });
+
+  it('should display all card suits', () => {
+    suitList.forEach((suit) => {
+      const suitImg = fixture.debugElement.query(
+        By.css(`img[src="/assets/card_${suit.toLowerCase()}.svg"]`)
+      );
+      expect(suitImg).toBeTruthy();
+    });
+  });
+
+  const clickSuitCard = (suit: Suit): HTMLImageElement | null => {
+    const suitImg = fixture.debugElement
+      .queryAll(By.css('img'))
+      .map((img) => img.nativeElement as HTMLImageElement)
+      .find(
+        (img) =>
+          img.src.split('/assets/')[1] === `card_${suit.toLowerCase()}.svg`
+      );
+
+    suitImg?.click();
+
+    return suitImg ?? null;
+  };
+
+  it('should set select class on clubs when clicked', async () => {
+    const clubsImg = clickSuitCard(SUIT.CLUBS);
+    expect(clubsImg?.classList.contains('selected')).toBeTruthy();
+  });
+
+  it('should set select class on diamonds when clicked', async () => {
+    const diamondsImg = clickSuitCard(SUIT.DIAMONDS);
+    expect(diamondsImg?.classList.contains('selected')).toBeTruthy();
+  });
+
+  it('should set select class on hearts when clicked', async () => {
+    const heartsImg = clickSuitCard(SUIT.HEARTS);
+    expect(heartsImg?.classList.contains('selected')).toBeTruthy();
+  });
+
+  it('should set select class on spades when clicked', async () => {
+    const spadesImg = clickSuitCard(SUIT.SPADES);
+    expect(spadesImg?.classList.contains('selected')).toBeTruthy();
+  });
+
+  it('should select suit on suit click', async () => {
+    spyOn(component, 'selectSuit').and.callThrough();
+    spyOn((component as any).introFacade, 'setSuit');
+
+    const images = fixture.debugElement
+      .queryAll(By.css('img'))
+      .map((img) => img.nativeElement as HTMLImageElement);
+
+    suitList.forEach(async (suit) => {
+      const suitImage = images.find((image) => {
+        const imgSrc = image.src.split('/assets/')[1];
+        return imgSrc === `card_${suit.toLowerCase()}.svg`;
+      });
+
+      suitImage?.click();
+
+      fixture.detectChanges();
+      await fixture.whenStable();
+
+      expect(component.selectSuit).toHaveBeenCalledWith(suit);
+      expect((component as any).introFacade.setSuit).toHaveBeenCalledWith(suit);
+    });
   });
 });
--- client/src/app/ui/intro/containers/intro-page/intro-page.component.spec.ts
+++ client/src/app/ui/intro/containers/intro-page/intro-page.component.spec.ts
@@ -3,6 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { By } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BASE_API_URL_KEY } from '@constants/base-api-url-key';
 import { provideMockStore } from '@ngrx/store/testing';
@@ -49,5 +50,12 @@ describe('IntroPageComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should has the options component', () => {
+    const optionsComponent = fixture.debugElement.query(
+      By.directive(OptionsComponent)
+    );
+    expect(optionsComponent).toBeTruthy();
   });
 });
