50620fe41fb1af3443df4239f07a4cc818a2a345
CharlieGitDB <charliedeveloperaccess@gmail.com>
2023-03-03 21:36:16 -0600

    create post-game-page component

---
+++ client/src/app/ui/post-game/containers/post-game-page/post-game-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-game-page',
+  templateUrl: './post-game-page.component.html',
+  styleUrls: ['./post-game-page.component.scss']
+})
+export class PostGamePageComponent {
+
+}
