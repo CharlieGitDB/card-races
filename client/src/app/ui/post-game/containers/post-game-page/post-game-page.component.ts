db60cb96b3db34655f6e5424942066dcb9e35aec
CharlieGitDB <charliedeveloperaccess@gmail.com>
2023-03-06 20:29:34 -0600

    add postgame facade and replay buttons to postgame component

--- client/src/app/ui/post-game/containers/post-game-page/post-game-page.component.ts
+++ client/src/app/ui/post-game/containers/post-game-page/post-game-page.component.ts
@@ -1,15 +1,34 @@
-import { Component, inject } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  inject,
+} from '@angular/core';
 import { PostGameFacade } from '../../facades/post-game.facade';
 
 @Component({
   selector: 'app-post-game-page',
   templateUrl: './post-game-page.component.html',
   styleUrls: ['./post-game-page.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PostGamePageComponent {
   private postGameFacade = inject(PostGameFacade);
+  private cdr = inject(ChangeDetectorRef);
 
   public winner$ = this.postGameFacade.winner$;
 
   public winners$ = this.postGameFacade.winners$;
+
+  public loading = false;
+
+  public async replayGame() {
+    this.loading = true;
+    this.cdr.detectChanges();
+    await this.postGameFacade.replayGame();
+  }
+
+  public goToIntroPage() {
+    this.postGameFacade.goToIntroPage();
+  }
 }
--- client/src/app/ui/post-game/facades/post-game.facade.ts
+++ client/src/app/ui/post-game/facades/post-game.facade.ts
@@ -1,6 +1,10 @@
 import { inject, Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
+import { lastValueFrom, take } from 'rxjs';
 import {
+  ReplayGame,
+  selectGameGroupId,
   selectGameWinner,
   selectGameWinners,
   selectUserIsWinner,
@@ -12,8 +16,20 @@ import { AppState } from 'src/app/data/types/AppState';
 })
 export class PostGameFacade {
   private store: Store<AppState> = inject(Store);
+  private router = inject(Router);
 
   public isWinner$ = this.store.select(selectUserIsWinner);
   public winner$ = this.store.select(selectGameWinner);
   public winners$ = this.store.select(selectGameWinners);
+
+  public async replayGame() {
+    const group = await lastValueFrom(
+      this.store.select(selectGameGroupId).pipe(take(1))
+    );
+    this.store.dispatch(ReplayGame({ group }));
+  }
+
+  public goToIntroPage() {
+    this.router.navigate(['/']);
+  }
 }
